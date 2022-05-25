3e01adbb469ddd3853ecc440a18b3e9d27eccce4
unknown <semtehrani@yahoo.com>
2022-05-24 23:05:07 -0700

    started game.py welcome , game rules

---
+++ game.py
@@ -0,0 +1,26 @@
+# 5/24/2022  , rev .1 : 
+
+
+
+def greeting():
+    print()
+    print("*** Welcome to Rock-Paper-Scissors-Lizard-Spock  by Semi ***")
+    print()
+
+
+
+def rpsls_rules():
+    print()
+    print("Rules for Rock-Paper-Scissors-Lizard-Spock : ")
+    print()
+    print("Scissors cuts Paper")
+    print("Paper covers Rock")
+    print("Rock crushes Lizard")
+    print("Lizard poisons Spock")
+    print("Spock smashes Scissors")
+    print("Scissors decapitates Lizard")
+    print("Lizard eats Paper")
+    print("Paper disproves Spock")
+    print("Spock vaporizes Rock")
+    print("Rock crushes Scissors")
+    print()
