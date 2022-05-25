9357679b5059affef1b668ad72bee455d21e3726
unknown <semtehrani@yahoo.com>
2022-05-24 22:54:40 -0700

    ai.py code added

---
+++ ai.py
@@ -0,0 +1,24 @@
+# 5/24/2022 rev.1 : copied from Demo video
+
+
+
+
+
+
+from player import Player
+import random
+from time import sleep
+
+class AI(Player) : 
+
+    def __init__(self, name) :
+        super().__init__()
+        self.score = 0
+        self.name = name
+
+
+    def choose_guesture(self) : 
+        self.chosen_gesture = str(random.randint(0,4))
+        gesture_list = ["Rock", "Paper", "Scissors" , "Lizard" , "Spock"]
+        sleep(1)
+        print(f"{self.name} has picked {gesture_list[int(self.chosen_gesture)]}")
